0e86e73695b1c5572c5ec650341dbfbe09561c3d
variantxx <juanitobaldoaspler@gmail.com>
2026-03-28 20:58:00 +0800

    home updated

--- app/src/main/java/com/variantxx/codefest/HomeActivity.java
+++ app/src/main/java/com/variantxx/codefest/HomeActivity.java
@@ -1,26 +1,25 @@
 package com.variantxx.codefest;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
-
-
-    ListView listView;
+    DBHandler dbHandler;
     SharedPreferences sharedPref;
-
-
+    ArrayList<Note> arrayList;
+    NoteViewAdapter arrayAdapter;
+    ListView listView;
+    Button addNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,32 +29,31 @@ public class HomeActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
-            DBHandler dbHandler = new DBHandler(this);
-            listView = findViewById(R.id.listView);
-
-            ArrayList<Note> arrayList = new ArrayList<>();
-            NoteViewAdapter arrayAdapter = new NoteViewAdapter(this, arrayList);
-            listView.setAdapter(arrayAdapter);
-
-
-            sharedPref = getSharedPreferences("CodeFestPref", MODE_PRIVATE);
-            int userId = sharedPref.getInt("userId", -1);
-            List<Note> notes = dbHandler.getNotesByUser(userId);
-            arrayList.clear();
-            arrayList.addAll(notes);
-
-
-
-
-
-
-
-
-
-
-
             return insets;
         });
+
+        dbHandler = new DBHandler(this);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new NoteViewAdapter(this, arrayList);
+        addNoteBtn = findViewById(R.id.home_add_note_btn);
+        listView = findViewById(R.id.listView);
+        sharedPref = getSharedPreferences("CodeFestPref", MODE_PRIVATE);
+
+        initialize();
     }
+
+    private void initialize() {
+        // Display data to list view.
+        listView.setAdapter(arrayAdapter);
+        int userId = sharedPref.getInt("userId", -1);
+        List<Note> notes = dbHandler.getNotesByUser(userId);
+        arrayList.clear();
+        arrayList.addAll(notes);
+
+        addNoteBtn.setOnClickListener(v -> {
+            Intent i = new Intent(this, NoteAddScreenActivity.class);
+            startActivity(i);
+        });
+    }
+
 }
--- app/src/main/java/com/variantxx/codefest/NoteAddScreenActivity.java
+++ app/src/main/java/com/variantxx/codefest/NoteAddScreenActivity.java
@@ -1,6 +1,11 @@
 package com.variantxx.codefest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class NoteAddScreenActivity extends AppCompatActivity {
+    DBHandler dbHandler;
+    SharedPreferences sharedPref;
+    EditText titleInput, contentInput;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,32 @@ public class NoteAddScreenActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        dbHandler = new DBHandler(this);
+        sharedPref = getSharedPreferences("CodeFestPref", MODE_PRIVATE);
+        titleInput = findViewById(R.id.add_note_title_input);
+        contentInput = findViewById(R.id.add_note_content_input);
+        saveButton = findViewById(R.id.add_note_btn);
+
+        initialize();
+    }
+
+    private void initialize() {
+        saveButton.setOnClickListener(v -> {
+            String title = titleInput.getText().toString();
+            String content = contentInput.getText().toString();
+
+            if (!title.isEmpty() && !content.isEmpty()) {
+                int userId = sharedPref.getInt("user_id", -1);
+                if (userId != -1) {
+                    dbHandler.addNote(userId, title, content);
+                    Toast.makeText(this, "Note added successfully!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(this, HomeActivity.class);
+                    startActivity(i);
+                }
+                finish();
+            }
         });
     }
 }
