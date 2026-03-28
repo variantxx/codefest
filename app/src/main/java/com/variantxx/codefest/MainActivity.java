9b8a7b862266a31e806bf72e8dd902893fd11105
variantxx <juanitobaldoaspler@gmail.com>
2026-03-28 19:51:13 +0800

    login updated

--- app/src/main/java/com/variantxx/codefest/MainActivity.java
+++ app/src/main/java/com/variantxx/codefest/MainActivity.java
@@ -1,6 +1,11 @@
 package com.variantxx.codefest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     DBHandler dbHandler;
+    SharedPreferences sharedPref;
+    EditText emailInput, passwordInput;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +28,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Initialize the database handler
         dbHandler = new DBHandler(this);
+        sharedPref = getSharedPreferences("CodeFestPref", MODE_PRIVATE);
+        emailInput = findViewById(R.id.login_email_input);
+        passwordInput = findViewById(R.id.login_password_input);
+        loginBtn = findViewById(R.id.login_btn);
+
+        initialize();
+    }
+
+    private void initialize() {
+        // Initialize the database handler.
         dbHandler.getWritableDatabase();
+
+        // Handle user login
+        loginBtn.setOnClickListener(v -> {
+            String email = emailInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            if (!dbHandler.checkUser(email, password)) {
+                Toast.makeText(this, "Invalid email or password!", Toast.LENGTH_SHORT).show();
+            }else {
+                // Mark user as logged in.
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("userEmail", email);
+                editor.apply();
+                // Navigate to main screen.
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
